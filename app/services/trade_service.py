089d71eba74b648b8bce2c6bbb7c5d2ae9aad389
kimdaewhi <kimdaewhi@naver.com>
2026-03-19 18:17:01 +0900

    ♻️ refactor(order): 매수 주문 예외 처리 로직 개선

--- app/services/trade_service.py
+++ app/services/trade_service.py
@@ -1,9 +1,11 @@
 import datetime
 from fastapi import HTTPException
+import httpx
 
 from app.broker.kis.enums import ORD_DVSN_KRX, EXCG_ID_DVSN_CD
 from app.broker.kis.kis_order import KISOrder
 from app.core.enums import OrderType
+from app.core.exceptions import KISOrderError
 from app.core.settings import settings
 from app.schemas.kis import OrderResponse
 from app.utils.logger import get_logger
@@ -61,21 +63,87 @@ class TradeService:
                 price=normalized_price,
                 exchange_type=EXCG_ID_DVSN_CD.KRX.value,
             )
-            if order_response is None:
-                logger.error("매수 체결 실패: API 응답이 None입니다.")
-                raise HTTPException(status_code=500, detail="매수 체결 처리 중 오류가 발생했습니다.")
-            elif order_response.rt_cd != "0":
-                logger.error(f"매수 체결 실패: API 응답 코드 {order_response.rt_cd}, 메시지 코드: {order_response.msg_cd}")
-                raise HTTPException(status_code=400, detail=f"매수 체결 실패: {order_response.msg1}")
             
             today = datetime.datetime.now()
             order_datetime = today.replace(hour=int(order_response.output.ORD_TMD[:2]), minute=int(order_response.output.ORD_TMD[2:4]), second=int(order_response.output.ORD_TMD[4:6]))
             logger.info(f"매수 체결 성공 - 주문 번호: {order_response.output.ODNO}, 주문 시간 : {order_datetime}, KRX 전송주문번호 : {order_response.output.KRX_FWDG_ORD_ORGNO}")
             
             return order_response
+        
+        # ❌ 재시도 대상 (네트워크 계열)
+        except (httpx.HTTPError, httpx.TimeoutException) as e:
+            logger.error(f"주문 실패 (네트워크 오류): {e}")
+            raise HTTPException(
+                status_code=503,
+                detail="매수 체결 요청 중 네트워크 오류가 발생했습니다."
+            )
+        
+        # ❌ 주문 거절 (브로커에서 올라온 에러)
+        except KISOrderError as e:
+            logger.error(f"주문 실패 (거절): {e}")
+            raise HTTPException(
+                status_code=400,
+                detail=e.message
+            )
+        
+        # ❌ 기타 예외
         except Exception as e:
-            logger.error(f"매수 주문 중 오류 발생: {e}")
-            raise HTTPException(status_code=500, detail="매수 주문 처리 중 오류가 발생했습니다.")
+            logger.error(f"예상치 못한 오류: {e}")
+            raise HTTPException(
+                status_code=500,
+                detail="매수 주문 처리 중 오류 발생"
+            )
+
+        # TODO: 실제 운영에서는 네트워크 오류, 브로커 거절 등 다양한 실패 시나리오에 대비한 재시도 로직과 예외 처리를 구현하는 것이 좋음. 아래는 간단한 예시 코드.
+        # max_retry = 3
+        # delay = 0.5
+        
+        # for attempt in range(max_retry):
+        #     try:
+        #         # 2. KISOrder 클래스의 매수 메서드 호출, 주문 체결 요청
+        #         order_response =  await self.kis_order.buy_domestic_stock_by_cash(
+        #             access_token=access_token,
+        #             account_no=settings.KIS_ACCOUNT_NO,
+        #             account_product_code=settings.KIS_ACCOUNT_PRODUCT_CODE,
+        #             order_type=order_mode,
+        #             stock_code=stock_code,
+        #             quantity=quantity,
+        #             price=normalized_price,
+        #             exchange_type=EXCG_ID_DVSN_CD.KRX.value,
+        #         )
+                
+        #         today = datetime.datetime.now()
+        #         order_datetime = today.replace(hour=int(order_response.output.ORD_TMD[:2]), minute=int(order_response.output.ORD_TMD[2:4]), second=int(order_response.output.ORD_TMD[4:6]))
+        #         logger.info(f"매수 체결 성공 - 주문 번호: {order_response.output.ODNO}, 주문 시간 : {order_datetime}, KRX 전송주문번호 : {order_response.output.KRX_FWDG_ORD_ORGNO}")
+            
+        #         return order_response
+            
+        #     # ❌ 재시도 대상 (네트워크 계열)
+        #     except (httpx.HTTPError, httpx.TimeoutException) as e:
+        #         logger.warning(f"[{attempt+1}/{max_retry}] 네트워크 오류: {e}")
+        #         if attempt == max_retry - 1:
+        #             logger.error("재시도 실패 - 주문 불확실 상태")
+        #             raise HTTPException(
+        #                 status_code=500,
+        #                 detail="주문 요청 실패 (네트워크 오류)"
+        #             )
+        #         await asyncio.sleep(delay * (attempt + 1))
+            
+        #     # ❌ 주문 거절 (브로커에서 올라온 에러)
+        #     except KISOrderError as e:
+        #         logger.error(f"주문 실패 (거절): {e}")
+        #         raise HTTPException(
+        #             status_code=400,
+        #             detail=e.message
+        #         )
+            
+        #     # ❌ 기타 예외
+        #     except Exception as e:
+        #         logger.error(f"예상치 못한 오류: {e}")
+        #         raise HTTPException(
+        #             status_code=500,
+        #             detail="매수 주문 처리 중 오류 발생"
+        #         )
     
     
     # ⚙️ 국내 주식 현금 매도 체결 요청
