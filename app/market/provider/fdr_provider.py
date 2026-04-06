08ef96dfa3e8df3c83aafce2a22f8bf271418f9e
kimdaewhi <kimdaewhi@naver.com>
2026-04-06 23:08:26 +0900

    🔧 chore(settings): jupyter패키지 추가

--- app/market/provider/fdr_provider.py
+++ app/market/provider/fdr_provider.py
@@ -5,7 +5,13 @@ from .base_provider import BaseMarketDataProvider
 
 
 class FDRMarketDataProvider(BaseMarketDataProvider):
-
+    
+    # ⚙️ 종목 리스트 조회(한국거래소 상장 종목) - 원본 데이터프레임 반환
+    def get_stock_list_raw(self) -> pd.DataFrame:
+        return fdr.StockListing('KRX')
+    
+    
+    # ⚙️ 종목 리스트 조회(한국거래소 상장 종목)
     def get_stock_list(self) -> pd.DataFrame:
         df = fdr.StockListing('KRX')
         return df[['Code', 'Name', 'Market']].rename(columns={
@@ -13,7 +19,9 @@ class FDRMarketDataProvider(BaseMarketDataProvider):
             'Name': 'name',
             'Market': 'market'
         })
-
+    
+    
+    # ⚙️ 시장 데이터 조회(Open, High, Low, Close, Volume)
     def get_ohlcv(self, code: str, start: str, end: str) -> pd.DataFrame:
         df = fdr.DataReader(code, start, end)
         return df.reset_index()
